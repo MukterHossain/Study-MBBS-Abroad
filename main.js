e88c2c9c7d8e1f12ac32fa163aee74b6e4873ba4
MukterHossain <mukter3075@gmail.com>
2025-02-14 17:12:19 +0600

    added Appy Form and Form Validation

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
 
-const data = document.getElementById('new')
-const color = data.style.color='red'
+// Form Validation
+document.getElementById('applyForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    let name = document.getElementById('name').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let phone = document.getElementById('phone').value.trim();
+    let country = document.getElementById('country').value;
+    if(!name || !email || !phone || !country){
+        alert("plese fill in all fields")
+    }
+    alert("Application submitted successfully!")
+})
